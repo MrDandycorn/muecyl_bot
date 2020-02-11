07ab61a002e985908cdd7396921961e302e11d58
Kuza <bghj25@yajndex.ru>
2020-02-12 00:39:02 +0300

    suggestions work

--- main.py
+++ main.py
@@ -26,7 +26,14 @@ except Exception as exc:
 def mainmenu():
     keyboard = vk_botting.Keyboard()
     keyboard.add_button('Заполнить заново', vk_botting.KeyboardColor.PRIMARY)
-    keyboard.add_button('Искать людей', vk_botting.KeyboardColor.PRIMARY)
+    keyboard.add_button('Искать', vk_botting.KeyboardColor.PRIMARY)
+    return keyboard
+
+
+def like_menu():
+    keyboard = vk_botting.Keyboard()
+    keyboard.add_button('Топчег', vk_botting.KeyboardColor.PRIMARY)
+    keyboard.add_button('Нахуй', vk_botting.KeyboardColor.PRIMARY)
     return keyboard
 
 
@@ -60,7 +67,7 @@ async def user_registration(ctx):
     if msg.text.lower() == 'мужской':
         user_info['user_sex'] = 1
     elif msg.text.lower() == 'женский':
-        user_info['user_name'] = 2
+        user_info['user_sex'] = 2
 
     await ctx.send('Кого ищешь?', keyboard=search_sex_menu())
     msg = await bot.wait_for('message_new', check=verefy, timeout=3600)
@@ -73,6 +80,11 @@ async def user_registration(ctx):
     msg = await bot.wait_for('message_new', check=verefy, timeout=3600)
     user_info['description'] = msg.text
     cursor = con.cursor()
+    cursor.execute('SELECT * FROM users WHERE user_id=%s', [user_info['user_id']])
+    users = cursor.fetchall()
+    if users != ():
+        cursor.execute(f'DELETE FROM users WHERE user_id=%s', [user_info['user_id']])
+    print(user_info)
     cursor.execute(f'INSERT INTO users (user_id, user_name, user_sex, search_sex, description) '
                            f'VALUES (%s, %s, %s, %s, %s)',
                            [user_info['user_id'], user_info['user_name'], user_info['user_sex'],
@@ -117,6 +129,53 @@ async def begin(ctx):
             await show_user_form(ctx)
 
 
+def update_user_suggestions(user_id, user_sex, search_sex):
+    cursor = con.cursor()
+    cursor.execute('SHOW TABLES in muecyl')
+    tables = cursor.fetchall()
+    print(tables[0])
+    table_exist = False
+    for table in tables:
+        if table['Tables_in_muecyl'] == ('s' + str(user_id)):
+            table_exist = True
+            break
+    if not table_exist:
+        cursor.execute(f'CREATE TABLE s%s (suggestion_id BIGINT)', [user_id])
+    cursor.execute('SELECT * FROM users')
+    users = cursor.fetchall()
+    for user in users:
+        print(user['user_id'])
+        print(user_id)
+        print(user['user_sex'])
+        print(search_sex)
+        print(user['search_sex'])
+        print(user_sex)
+        cursor.execute(f'SELECT * FROM s%s WHERE suggestion_id = %s', [user_id, user['user_id']])
+        if (cursor.fetchall() == ()) and (user['user_id'] != user_id) and (user['user_sex'] == search_sex) and (user['search_sex'] == user_sex):
+            cursor.execute(f'INSERT INTO s%s (suggestion_id) VALUES (%s)', [user_id, user['user_id']])
+    cursor.close()
+
+
+@bot.command(name='искать')
+async def search(ctx):
+    cursor = con.cursor()
+    cursor.execute(f'SELECT * FROM users WHERE user_id =%s', [ctx.from_id])
+    user_info = cursor.fetchone()
+    cursor.close()
+    update_user_suggestions(ctx.from_id, user_info['user_sex'], user_info['search_sex'])
+    cursor = con.cursor()
+    cursor.execute(f'SELECT * FROM s%s', [ctx.from_id])
+    suggested_users = cursor.fetchall()
+    cursor.close()
+    print(suggested_users)
+    cur_user = 0;
+    for user in suggested_users:
+        cursor = con.cursor()
+        cursor.execute(f'SELECT * FROM users WHERE user_id = %s', [user['suggestion_id']])
+        user_to_suggest = cursor.fetchone()
+        print(user_to_suggest)
+        cursor.close()
+        await ctx.send('Нашел для тебя\n' + user_to_suggest['user_name'] + '\n' + user_to_suggest['description'], keyboard=like_menu())
 con = sqlpool.get_conn()
 if not con.open:
     con.ping(True)
