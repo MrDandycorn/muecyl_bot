5e8728af708a2fa1ee83c50240b76de8364a7e13
Kuza <bghj25@yajndex.ru>
2020-02-12 03:01:51 +0300

    queue added

--- main.py
+++ main.py
@@ -85,8 +85,8 @@ async def user_registration(ctx):
     if users != ():
         cursor.execute(f'DELETE FROM users WHERE user_id=%s', [user_info['user_id']])
     print(user_info)
-    cursor.execute(f'INSERT INTO users (user_id, user_name, user_sex, search_sex, description) '
-                           f'VALUES (%s, %s, %s, %s, %s)',
+    cursor.execute(f'INSERT INTO users (user_id, user_name, user_sex, search_sex, description, next_user) '
+                           f'VALUES (%s, %s, %s, %s, %s, 0)',
                            [user_info['user_id'], user_info['user_name'], user_info['user_sex'],
                             user_info['search_sex'], user_info['description']])
     cursor.close()
@@ -133,7 +133,6 @@ def update_user_suggestions(user_id, user_sex, search_sex):
     cursor = con.cursor()
     cursor.execute('SHOW TABLES in muecyl')
     tables = cursor.fetchall()
-    print(tables[0])
     table_exist = False
     for table in tables:
         if table['Tables_in_muecyl'] == ('s' + str(user_id)):
@@ -144,12 +143,12 @@ def update_user_suggestions(user_id, user_sex, search_sex):
     cursor.execute('SELECT * FROM users')
     users = cursor.fetchall()
     for user in users:
-        print(user['user_id'])
-        print(user_id)
-        print(user['user_sex'])
-        print(search_sex)
-        print(user['search_sex'])
-        print(user_sex)
+        # print(user['user_id'])
+        # print(user_id)
+        # print(user['user_sex'])
+        # print(search_sex)
+        # print(user['search_sex'])
+        # print(user_sex)
         cursor.execute(f'SELECT * FROM s%s WHERE suggestion_id = %s', [user_id, user['user_id']])
         if (cursor.fetchall() == ()) and (user['user_id'] != user_id) and (user['user_sex'] == search_sex) and (user['search_sex'] == user_sex):
             cursor.execute(f'INSERT INTO s%s (suggestion_id) VALUES (%s)', [user_id, user['user_id']])
@@ -162,20 +161,57 @@ async def search(ctx):
     cursor.execute(f'SELECT * FROM users WHERE user_id =%s', [ctx.from_id])
     user_info = cursor.fetchone()
     cursor.close()
-    update_user_suggestions(ctx.from_id, user_info['user_sex'], user_info['search_sex'])
-    cursor = con.cursor()
-    cursor.execute(f'SELECT * FROM s%s', [ctx.from_id])
-    suggested_users = cursor.fetchall()
-    cursor.close()
-    print(suggested_users)
-    cur_user = 0;
-    for user in suggested_users:
+    stop = False
+    while not stop:
+        update_user_suggestions(ctx.from_id, user_info['user_sex'], user_info['search_sex'])
         cursor = con.cursor()
-        cursor.execute(f'SELECT * FROM users WHERE user_id = %s', [user['suggestion_id']])
-        user_to_suggest = cursor.fetchone()
-        print(user_to_suggest)
+        cursor.execute(f'SELECT * FROM s%s', [ctx.from_id])
+        suggested_users = cursor.fetchall()
         cursor.close()
-        await ctx.send('Нашел для тебя\n' + user_to_suggest['user_name'] + '\n' + user_to_suggest['description'], keyboard=like_menu())
+        print(suggested_users)
+        cur_user = user_info['next_user']
+        print(cur_user)
+        cur_user = int(cur_user)
+        for user in suggested_users:
+            cur_user += 1
+            cursor = con.cursor()
+            cursor.execute(f'SELECT * FROM users WHERE user_id = %s', [user['suggestion_id']])
+            user_to_suggest = cursor.fetchone()
+            cursor.execute(f'UPDATE users SET next_user =%s WHERE user_id=%s', [cur_user, ctx.from_id])
+            cursor.close()
+            await ctx.send('Нашел для тебя\n' + user_to_suggest['user_name'] + '\n' + user_to_suggest['description'],
+                           keyboard=like_menu())
+
+            def verefy(message):
+                return message.from_id == ctx.from_id
+            msg = await bot.wait_for('message_new', check=verefy, timeout=3600)
+            if msg.text.lower() == 'топчег':
+                cursor = con.cursor()
+                cursor.execute('SHOW TABLES in muecyl')
+                tables = cursor.fetchall()
+                table_exist = False
+                for table in tables:
+                    if table['Tables_in_muecyl'] == ('q' + str(user_to_suggest['user_id'])):
+                        table_exist = True
+                        break
+                if not table_exist:
+                    cursor.execute(f'CREATE TABLE q%s (id BIGINT)', user_to_suggest['user_id'])
+                cursor.execute('SELECT * FROM q%s', user_to_suggest['user_id'])
+                queue = cursor.fetchall()
+                if queue == ():
+                    pass#избранный пользваатель еще не плучил не одного сообщения, очередь пустая, отправляем ему
+                cursor.execute(f'SELECT * FROM q%s WHERE id = %s', [user_to_suggest['user_id'], ctx.from_id])
+                if cursor.fetchall() == ():
+                    cursor.execute(f'INSERT INTO q%s (id) VALUES (%s)', [user_to_suggest['user_id'], ctx.from_id])
+                cursor.close()
+            if msg.text.lower() == 'стоп':  # вынести в отельную команду
+                stop = True
+                await ctx.send('Возвращайся!', keyboard=mainmenu())
+
+
+@bot.command(name='стоп')
+async def restart(ctx):
+    await show_user_form(ctx)
 con = sqlpool.get_conn()
 if not con.open:
     con.ping(True)
