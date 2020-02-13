59c5f90e24b48c354cbc43d68d29fe5e56185460
Kuza <bghj25@yajndex.ru>
2020-02-14 02:44:02 +0300

    beta version

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import credentials as cred
 import vk_botting
 from pymysqlpool.pool import Pool  # Для работы с сервером и БД
 import pymysql
-import aiomysql
+import datetime
 bot = vk_botting.Bot(vk_botting.when_mentioned_or_pm(), case_insensitive=True)
 config = {'host': cred.host, 'user': cred.user, 'password': cred.password, 'db': cred.db, 'autocommit': True, 'charset': cred.charset, 'cursorclass': pymysql.cursors.DictCursor}
 try:
@@ -149,7 +149,7 @@ async def suggest(ctx):
     done = False
     for possible_user in possible_users:
         if str(possible_user['user_id']) not in already_suggested:
-            await ctx.send('Нашел для тебя:\n{}\n{}'.format(possible_user['user_name'], possible_user['description']))
+            await ctx.send('Нашел для тебя:\n{}\n{}'.format(possible_user['user_name'], possible_user['description']), keyboard=like_menu())
             already_suggested.append(str(possible_user['user_id']))
             already_suggested = 's'.join(already_suggested)
             cursor.execute('UPDATE users SET suggested_users = \'{}\' WHERE user_id = {}'.format(already_suggested, ctx.from_id))
@@ -157,9 +157,8 @@ async def suggest(ctx):
             done = True
             break
     if not done:
-        await ctx.send('Нет подходящих')
         cursor.execute('UPDATE users SET last_suggestion = -1 WHERE user_id = {}'.format(ctx.from_id))
-    #print(possible_users)
+        await ctx.send('Нет подходящих', keyboard=mainmenu())
     cursor.close()
 
 
@@ -191,13 +190,13 @@ async def topcheg(ctx):
             queue = suggestion['queue'].split('s')
         else:
             queue = list()
-        print(len(queue))
         if len(queue) == 0:
             print('sendind...')
-            await bot.send_message(peer_id=user['last_suggestion'], message='Тебя оценили\n{}\n{}'.format(user['user_name'], user['description']))
-        queue.append(str(user['user_id']))
-        queue = 's'.join(queue)
-        cursor.execute('UPDATE users SET queue = \'{}\' WHERE user_id={}'.format(queue, user['last_suggestion']))
+            await bot.send_message(peer_id=user['last_suggestion'], message='Тебя оценили\n{}\n{}'.format(user['user_name'], user['description']), keyboard=answer_menu())
+        if str(ctx.from_id) not in queue:
+            queue.append(str(user['user_id']))
+            queue = 's'.join(queue)
+            cursor.execute('UPDATE users SET queue = \'{}\' WHERE user_id={}'.format(queue, user['last_suggestion']))
     cursor.close()
     await suggest(ctx)
 
@@ -207,96 +206,19 @@ async def nahui(ctx):
     await suggest(ctx)
 
 
-# def update_user_suggestions(user_id, user_sex, search_sex):
-#     cursor = con.cursor()
-#     cursor.execute('SHOW TABLES in muecyl')
-#     tables = cursor.fetchall()
-#     table_exist = False
-#     for table in tables:
-#         if table['Tables_in_muecyl'] == ('s' + str(user_id)):
-#             table_exist = True
-#             break
-#     if not table_exist:
-#         cursor.execute(f'CREATE TABLE s%s (suggestion_id BIGINT)', [user_id])
-#     cursor.execute('SELECT * FROM users')
-#     users = cursor.fetchall()
-#     for user in users:
-#         # print(user['user_id'])
-#         # print(user_id)
-#         # print(user['user_sex'])
-#         # print(search_sex)
-#         # print(user['search_sex'])
-#         # print(user_sex)
-#         cursor.execute(f'SELECT * FROM s%s WHERE suggestion_id = %s', [user_id, user['user_id']])
-#         if (cursor.fetchall() == ()) and (user['user_id'] != user_id) and (user['user_sex'] == search_sex) and (user['search_sex'] == user_sex):
-#             cursor.execute(f'INSERT INTO s%s (suggestion_id) VALUES (%s)', [user_id, user['user_id']])
-#     cursor.close()
-#
-#
-# @bot.command(name='искать')
-# async def search(ctx):
-#     cursor = con.cursor()
-#     cursor.execute(f'SELECT * FROM users WHERE user_id =%s', [ctx.from_id])
-#     user_info = cursor.fetchone()
-#     cursor.close()
-#     stop = False
-#     while not stop:
-#         update_user_suggestions(ctx.from_id, user_info['user_sex'], user_info['search_sex'])
-#         cursor = con.cursor()
-#         cursor.execute(f'SELECT * FROM s%s', [ctx.from_id])
-#         suggested_users = cursor.fetchall()
-#         cursor.close()
-#         #print(suggested_users)
-#         cur_user = user_info['next_user']
-#         #print(cur_user)
-#         cur_user = int(cur_user)
-#         for user in suggested_users:
-#             cur_user += 1
-#             cursor = con.cursor()
-#             cursor.execute(f'SELECT * FROM users WHERE user_id = %s', [user['suggestion_id']])
-#             user_to_suggest = cursor.fetchone()
-#             cursor.execute(f'UPDATE users SET next_user =%s WHERE user_id=%s', [cur_user, ctx.from_id])
-#             cursor.close()
-#             await ctx.send('Нашел для тебя\n' + user_to_suggest['user_name'] + '\n' + user_to_suggest['description'],
-#                            keyboard=like_menu())
-#
-#             def verefy(message):
-#                 return message.from_id == ctx.from_id
-#             msg = await bot.wait_for('message_new', check=verefy, timeout=3600)
-#             if msg.text.lower() == 'топчег':
-#                 cursor = con.cursor()
-#                 cursor.execute('SHOW TABLES in muecyl')
-#                 tables = cursor.fetchall()
-#                 table_exist = False
-#                 for table in tables:
-#                     if table['Tables_in_muecyl'] == ('q' + str(user_to_suggest['user_id'])):
-#                         table_exist = True
-#                         break
-#                 if not table_exist:
-#                     cursor.execute(f'CREATE TABLE q%s (id BIGINT)', user_to_suggest['user_id'])
-#                 cursor.execute('SELECT * FROM q%s', user_to_suggest['user_id'])
-#                 queue = cursor.fetchall()
-#                 print(123445)
-#                 print(queue)
-#                 if queue == ():
-#                     print(45960)
-#                     print(user_info)
-#                     await bot.send_message(
-#                         peer_id=user_to_suggest['user_id'],
-#                         message=('Тебя оценили\n {} \n {}'.format(user_info['user_name'], user_info['description'])),
-#                         keyboard=answer_menu())
-#                 cursor.execute(f'SELECT * FROM q%s WHERE id = %s', [user_to_suggest['user_id'], ctx.from_id])
-#                 if cursor.fetchall() == ():
-#                     cursor.execute(f'INSERT INTO q%s (id) VALUES (%s)', [user_to_suggest['user_id'], ctx.from_id])
-#                 cursor.close()
-#             if msg.text.lower() == 'стоп':  # вынести в отельную команду
-#                 stop = True
-#                 await ctx.send('Возвращайся!', keyboard=mainmenu())
-
-
 @bot.command(name='стоп')
 async def restart(ctx):
-    #await ctx.send(' ', keyboard=mainmenu())
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM users WHERE user_id={}'.format(ctx.from_id))
+    user = cursor.fetchone()
+    if user['suggested_users']!='' and user['suggested_users'] is not None:
+        suggested = user['suggested_users'].split('s')
+        suggested.pop()
+        suggested = 's'.join(suggested)
+        print('UPDATE users SET suggested_users =\'{}\' WHERE user_id = {}'.format(suggested, ctx.from_id))
+        cursor.execute('UPDATE users SET suggested_users =\'{}\' WHERE user_id = {}'.format(suggested, ctx.from_id))
+    cursor.execute('UPDATE users SET last_suggestion =-1 WHERE user_id = {}'.format(ctx.from_id))
+    cursor.close()
     await show_user_form(ctx)
 
 
@@ -305,41 +227,69 @@ async def like(ctx):
     cursor = con.cursor()
     cursor.execute('SELECT * FROM users WHERE user_id={}'.format(ctx.from_id))
     user = cursor.fetchone()
-    if user['queue'] is not None:
+    if user['queue'] is not None and user['queue'] != '':
         queue = user['queue'].split('s')
         await bot.send_message(peer_id=queue[0], message='Добавляйся, vk.com/id{}'.format(ctx.from_id))
         await ctx.send('Добавляйся, vk.com/id{}'.format(queue[0]))
         queue.pop(0)
         queue = 's'.join(queue)
-        cursor.execute('UPDATE users SET queue = \'{}\' WHERE user_id = '.format(queue, user['user_id']))
+        cursor.execute('UPDATE users SET queue = \'{}\' WHERE user_id = {}'.format(queue, user['user_id']))
         cursor.close()
         await next_suggestion(ctx)
     else:
         cursor.close()
-        await ctx.send('информация не акутальна')
+        await ctx.send('информация не акутальна', keyboard=mainmenu())
         await show_user_form(ctx)
 
 
 @bot.command(name='нах')
 async def nah(ctx):
-    await next_suggestion(ctx)
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM users WHERE user_id={}'.format(ctx.from_id))
+    user = cursor.fetchone()
+    if user['queue'] is not None and user['queue'] != '':
+        queue = user['queue'].split('s')
+        queue.pop(0)
+        queue = 's'.join(queue)
+        cursor.execute('UPDATE users SET queue = \'{}\' WHERE user_id = {}'.format(queue, user['user_id']))
+        cursor.close()
+        await next_suggestion(ctx)
+    else:
+        cursor.close()
+        await ctx.send('информация не акутальна', keyboard=mainmenu())
+        await show_user_form(ctx)
 
 
 async def next_suggestion(ctx):
     cursor = con.cursor()
     cursor.execute('SELECT * FROM users WHERE user_id={}'.format(ctx.from_id))
     user = cursor.fetchone()
-    if user['queue'] is None:
+    print(user['queue'])
+    if user['queue'] is None or user['queue'] == '':
         cursor.close()
-        ctx.send('Больше нет заявок')
+        await ctx.send('Больше нет заявок')
+        await show_user_form(ctx)
     else:
         queue = user['queue'].split('s')
+        print(queue)
         cursor.execute('SELECT * FROM users WHERE user_id={}'.format(queue[0]))
         suggestion = cursor.fetchone()
-        await ctx.send('Тебя оценили\n{}\n{}'.format(suggestion['user_name'], suggestion['description']))
+        await ctx.send('Тебя оценили\n{}\n{}'.format(suggestion['user_name'], suggestion['description']), keyboard=answer_menu())
         cursor.close()
 
 
+# async def reset_suggestions():
+#     cursor = con.cursor()
+#     cursor.execute('UPDATE users SET suggested_users = \'\'')
+#     cursor.execute('UPDATE users SET last_suggestion = -1')
+#     cursor.close()
+#     print(datetime.datetime.now())
+#
+#
+# @bot.listen()
+# async def on_ready():
+#     bot.loop.create_task(reset_suggestions())
+#
 con = sqlpool.get_conn()
 if not con.open:
     con.ping(True)
